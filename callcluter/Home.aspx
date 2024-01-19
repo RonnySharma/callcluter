7c2e5e8055d6c7f7f3c6d22db72c0a2c9d94a3cf
Mohit Sharma <Mohit Sharma@PIYUSH>
2024-01-19 15:42:40 +0530

    Add project files.

---
+++ callcluter/Home.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="callcluter.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="welcome" />
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+<asp:Button ID="Button2" runat="server" OnClick="Button2_Click"  Text="sum" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button2_Click" Text="mul" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button2_Click"  Text="div" />
+        <br />
+        <br />
+        <asp:Button ID="Button5" runat="server" Text="Next Page" />
+    </form>
+</body>
+</html>
---
+++ callcluter/Home.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace callcluter
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Page.IsPostBack==false)
+            {
+                Label1.Text = DateTime.Now.ToString();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if(sender == Button2)
+            {
+                TextBox3.Text=Convert.ToString(Convert.ToInt32(TextBox1.Text) + Convert.ToInt32(TextBox2.Text));
+
+            }
+            else if(sender==Button3)
+            {
+                TextBox3.Text= Convert.ToString(Convert.ToInt32(TextBox1.Text) * Convert.ToInt32(TextBox2.Text));
+            }
+            else
+            {
+                TextBox3.Text= Convert.ToString(Convert.ToInt32(TextBox1.Text) / Convert.ToInt32(TextBox2.Text));
+            }
+        }
+    }
+}
